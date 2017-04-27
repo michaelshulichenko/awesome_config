81aaa842d8ab8d496deb28be09dd51d7f43de309
Michael Shulichenko <michael.shulichenko@cloudbeds.com>
2017-04-27 19:26:09 +0300

    Should work now :)

--- scripts/reset_xscreensaver.sh
+++ scripts/reset_xscreensaver.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Disable screensaver while zoom meeting is running
+export DISPLAY=:0
 
 ZOOM_MEETING_WINDOW="Zoom Meeting";
 ZOOM_IS_RUNNING=$(wmctrl -l | grep -i "${ZOOM_MEETING_WINDOW}");
 
-if [[ ! -z "$ZOOM_IS_RUNNING" ]]; then 
-	echo "Zoom meeting in process";
-	/usr/bin/xset s reset
+
+if [[ ! -z "${ZOOM_IS_RUNNING}" ]]; then
+	# echo "Zoom meeting in process";
+	xscreensaver-command -deactivate >&- 2>&- &
+else
+	# echo "No zoom meeting detected"
 fi
 
-exit 0;
+exit $?;
